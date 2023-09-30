1bec6a757941d8a3c5fd74a2ab5fc08d041a95a0
John Francis <john9francis@gmail.com>
2023-09-30 00:40:01 -0600

    now when you click the tower platform, the gui pops up, and when you click away it closes

--- scripts/towerPlatform.gd
+++ scripts/towerPlatform.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var mouse_in_tower = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,19 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_pressed("Click"):
+		if mouse_in_tower:
+			$TowerGUI.show()
+		else:
+			$TowerGUI.hide()
 	pass
+
+
+func _on_mouse_entered():
+	mouse_in_tower = true
+	pass # Replace with function body.
+
+
+func _on_mouse_exited():
+	mouse_in_tower = false
+	pass # Replace with function body.
