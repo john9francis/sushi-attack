6249edc843f9f436ea5925ec25b36fd4e1bdceb7
John Francis <john9francis@gmail.com>
2023-09-30 22:51:06 -0600

    added a timer to the w tower and hooked up some signals

--- scripts/w_tower.gd
+++ scripts/w_tower.gd
@@ -3,11 +3,28 @@ extends Area2D
 @export var ExplosionScene: PackedScene
 @export var TrackerScene: PackedScene
 
+var tracker
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	tracker = TrackerScene.instantiate()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+
+
+func _on_area_entered(area):
+	if area.is_in_group("Enemies"):
+		tracker.set_area(area)
+	pass # Replace with function body.
+
+
+func _on_area_exited(area):
+	if area.is_in_group("Enemies"):
+		tracker.unset_area()
+	pass # Replace with function body.
