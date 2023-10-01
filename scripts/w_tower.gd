83d360aa53ef7cc82dfefbe1d7ca5ae182f7567b
John Francis <john9francis@gmail.com>
2023-09-30 23:10:33 -0600

    wasabi tower now leaves behind wasabi bombs

--- scripts/w_tower.gd
+++ scripts/w_tower.gd
@@ -8,6 +8,7 @@ var tracker
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tracker = TrackerScene.instantiate()
+	add_child(tracker)
 	pass # Replace with function body.
 
 
@@ -21,10 +22,20 @@ func _process(delta):
 func _on_area_entered(area):
 	if area.is_in_group("Enemies"):
 		tracker.set_area(area)
-	pass # Replace with function body.
+		$ShootTimer.start()
 
 
 func _on_area_exited(area):
 	if area.is_in_group("Enemies"):
 		tracker.unset_area()
-	pass # Replace with function body.
+		$ShootTimer.stop()
+
+
+func _on_shoot_timer_timeout():
+	var enemyGlobalPosition = tracker.get_target_pos()
+	var enemyLocalPosition = to_local(enemyGlobalPosition)
+	
+	var explosion = ExplosionScene.instantiate()
+	explosion.position = enemyLocalPosition
+	
+	add_child(explosion)
