25e1e1fe86fa6436e088298738f7b13f85537854
John Francis <john9francis@gmail.com>
2023-10-02 11:41:36 -0600

    Now the bullets damage the enemies

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,10 +1,11 @@
 extends Area2D
 
-const speed = 5
-var currentSpeed = 5
 @export var myPathFollowScene: PackedScene
 var myPathFollow
 
+const speed = 5
+var currentSpeed = 5
+var health = 5
 
 func _ready():
 	add_to_group("Enemies")
@@ -12,7 +13,8 @@ func _ready():
 
 
 func _process(_delta):
-	pass
+	if health <= 0:
+		queue_free()
 	
 
 func set_speed(s):
@@ -30,3 +32,11 @@ func multiply_speed(s):
 	currentSpeed *= s
 	myPathFollow.set_speed(currentSpeed)
 	
+	
+
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Bullets"):
+		body.queue_free()
+		health -= 1
