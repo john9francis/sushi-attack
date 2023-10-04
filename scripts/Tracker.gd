a4d39d94da095114049576610753174f31c77b08
John Francis <john9francis@gmail.com>
2023-10-04 11:51:55 -0600

    fixed a bug with tracker, there was an inconsistency with the areaExists flag and the areaToTrack

--- scripts/Tracker.gd
+++ scripts/Tracker.gd
@@ -1,35 +1,34 @@
 extends Node
 
 var areaToTrack
-var areaExists
 
 var pastPosition
 var currentPosition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	areaExists = false
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if areaExists:
+	if areaToTrack != null:
 		pastPosition = currentPosition
 		currentPosition = areaToTrack.global_position
 
 
 func set_area(area):
 	areaToTrack = area
-	areaExists = true
 	
 	
 func unset_area():
 	areaToTrack = null
-	areaExists = false
 	
 func has_area():
-	return areaExists
+	if areaToTrack == null:
+		return false
+	else:
+		return true
 	
 	
 func get_target_pos():
