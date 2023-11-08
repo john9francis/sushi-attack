0500de260d05987a703bed0b589cab4b87e75b4a
John Francis <john9francis@gmail.com>
2023-11-07 23:21:42 -0700

    Got rid of some unneeded functions in enemyBuilder

--- scripts/enemy_builder.gd
+++ scripts/enemy_builder.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-# This node will create one single enemy and it's pathFollow. 
-#
 # The purpose of this class is because initializing an enemy is
 # complicated. We need an "enemyPathFollow" and an "enemy"
 # as well as setting the enemies speed, health, animation textures, etc. 
@@ -68,24 +66,7 @@ func get_premade_enemy(enemyName):
 	)
 	
 	return enemy
-	pass
 
-func set_enemy(_preloadedSpriteFrames, _speed, _health, _colissionRadius=null):
-	if enemy != null:
-		print("Error, an enemy has already been set")
-		return
-	
-	enemy = enemyScene.instantiate()
-	pathFollow = pathFollowScene.instantiate()
-	
-	enemy.set_path_follow(pathFollow)
-	enemy.set_speed(_speed)
-	enemy.set_health(_health)
-	
-	enemy.set_anim(_preloadedSpriteFrames)
-	enemy.set_sprite_size(_colissionRadius)
-	
-	pass
 
 func create_enemy(_preloadedSpriteFrames, _speed, _health, _colissionRadius=0):
 	var new_enemy = enemyScene.instantiate()
@@ -103,16 +84,4 @@ func create_enemy(_preloadedSpriteFrames, _speed, _health, _colissionRadius=0):
 	new_enemy.set_enemy_created_flag(true)
 	
 	return new_enemy
-
-func delete_enemy():
-	if enemy != null:
-		enemy.kill()
-
-
-func get_enemy():
-	if enemy != null:
-		return enemy
-	else:
-		print("enemy_builder Error: enemy not set correctly")
-
 
