e1a0553151fd4fa19ef766f5e29855b5658d0f30
John Francis <john9francis@gmail.com>
2023-11-17 22:17:01 -0700

    I figured it out, I need to make my own button scene

--- scripts/main_gui.gd
+++ scripts/main_gui.gd
@@ -7,6 +7,7 @@ extends CanvasLayer
 signal levelButtonPressed(levelName)
 
 var inLevelSelectFlag = false
+var currentButtonName = ""
 
 func _ready():
 	mainMenu.show()
@@ -14,8 +15,6 @@ func _ready():
 
 
 func _process(delta):
-	if Input.is_action_just_released("Click") and inLevelSelectFlag:
-		print("click")
 	pass
 
 
@@ -45,23 +44,8 @@ func populate_level_buttons(levelList):
 	for levelName in levelList:
 		var levelButton = Button.new()
 		levelButton.text = levelName
-		
-		# link up the button to go to that scene
-		levelButton.connect("pressed", _on_level_button_pressed)
-		#levelButton.pressed.connect(self._on_level_button_pressed)
-		
+
 		levelButtonContainer.add_child(levelButton)
 		
-		
-		
-func _on_level_button_pressed():
 	
-	var levelName = "test"
-	inLevelSelectFlag = false
-	print("clicked level")
-	
-	# This don't work:
-	#print("Button pressed:", get_node("Button").text)
-	
-	#print(levelName)
 
