ae404af3b1d7dfab775220a627f4e78d8b9ebe03
John Francis <john9francis@gmail.com>
2023-11-24 23:22:47 -0700

    Making the towergui buttons hoverable so when the mouse is over a button it displays the price

--- scripts/TowerGUI.gd
+++ scripts/TowerGUI.gd
@@ -1,5 +1,22 @@
 extends Control
 
+var csValue = 1
+var ssValue = 2
+var gValue = 3
+var wValue = 4
+
+@onready var csButton = $platformGUI/CSButton
+const csButtonText = "Chop Sticks"
+
+@onready var ssButton = $platformGUI/SSButton
+const ssButtonText = "Soy Sauce"
+
+@onready var wButton = $platformGUI/WButton
+const wButtonText = "Wasabi"
+
+@onready var gButton = $platformGUI/GButton
+const gButtonText = "Ginger"
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,3 +43,45 @@ func go_to_platform_gui():
 func _on_gui_input(event):
 	accept_event()
 	pass # Replace with function body.
+
+
+
+
+func _on_cs_button_mouse_entered():
+	csButton.text = csButtonText + '\n' + str(csValue)
+	pass # Replace with function body.
+
+func _on_cs_button_mouse_exited():
+	csButton.text = csButtonText
+	pass # Replace with function body.
+	
+	
+	
+func _on_ss_button_mouse_entered():
+	ssButton.text = ssButtonText + '\n' + str(ssValue)
+	pass # Replace with function body.
+
+func _on_ss_button_mouse_exited():
+	ssButton.text = ssButtonText
+	pass # Replace with function body.
+	
+	
+	
+func _on_g_button_mouse_entered():
+	gButton.text = gButtonText + '\n' + str(gValue)
+	pass # Replace with function body.
+
+func _on_g_button_mouse_exited():
+	gButton.text = gButtonText
+	pass # Replace with function body.
+	
+	
+	
+func _on_w_button_mouse_entered():
+	wButton.text = wButtonText + '\n' + str(wValue)
+	pass # Replace with function body.
+
+func _on_w_button_mouse_exited():
+	wButton.text = wButtonText
+	pass # Replace with function body.
+	
