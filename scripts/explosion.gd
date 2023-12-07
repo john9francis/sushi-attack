136f331c1d38a5fd303a9b34357b897d38598c59
John Francis <john9francis@gmail.com>
2023-12-06 21:28:49 -0700

    Added an anim to the explosion

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
+@onready var explosionAnim = $explosionAnim
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$DespawnTimer.start()
+	explosionAnim.play("explosion")
 	pass # Replace with function body.
 
 
@@ -13,5 +15,10 @@ func _process(delta):
 
 
 func _on_despawn_timer_timeout():
+	#queue_free()
+	pass # Replace with function body.
+
+
+func _on_explosion_anim_animation_finished():
 	queue_free()
 	pass # Replace with function body.
