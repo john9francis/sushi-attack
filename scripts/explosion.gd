d6f590a22694cf1734c7290b8e6ef559749055cd
John Francis <john9francis@gmail.com>
2023-12-06 21:49:58 -0700

    created a temporary path

--- scripts/explosion.gd
+++ scripts/explosion.gd
@@ -1,10 +1,9 @@
 extends Area2D
 
-@onready var explosionAnim = $explosionAnim
+@onready var explosionAnim = $CollisionShape2D/explosionAnim
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$DespawnTimer.start()
 	explosionAnim.play("explosion")
 	pass # Replace with function body.
 
@@ -12,11 +11,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_despawn_timer_timeout():
-	#queue_free()
-	pass # Replace with function body.
 
 
 func _on_explosion_anim_animation_finished():
