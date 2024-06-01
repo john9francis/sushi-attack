3eff9431fbe4bfed9324389ac198a0ad1f684c90
John Francis <john9francis@gmail.com>
2024-06-01 17:55:14 -0400

    Restored level 1 and updated the bento boxes to level 3

--- scripts/wave_list_level1.gd
+++ scripts/wave_list_level1.gd
@@ -9,13 +9,78 @@ const base_wave_entry = {
 # Setting up our enemy waves
 const wave1 = [
 	{
-		"enemyName": "bento_box",
+		"enemyName": "gyoza",
 		"amount": 4,
 		"timer": 3
+	},
+	{
+		"enemyName": "maki",
+		"amount": 5,
+		"timer": 2
+	},
+	{
+		"enemyName": "maki",
+		"amount": 5,
+		"timer": 1
+	}
+]
+const wave2 = [
+	{
+		"enemyName": "pink_maki",
+		"amount": 3,
+		"timer": 4
+	},
+	{
+		"enemyName": "maki",
+		"amount": 5,
+		"timer": 2
+	},
+	{
+		"enemyName": "pink_maki",
+		"amount": 7,
+		"timer": 3
+	},
+]
+
+const wave3 = [
+	{
+		"enemyName": "rice_ball",
+		"amount": 5,
+		"timer": 4
+	},
+	{
+		"enemyName": "pink_maki",
+		"amount": 7,
+		"timer": 3
+	},
+	{
+		"enemyName": "maki",
+		"amount": 10,
+		"timer": .5
+	},
+	{
+		"enemyName": "rice_ball",
+		"amount": 5,
+		"timer": 2
+	},
+	{
+		"enemyName": "pink_maki",
+		"amount": 7,
+		"timer": 1
 	}
 ]
 
-const wave_list = [wave1]
+const wave_list = [wave1, wave2, wave3]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
 
 
 func get_wave_list():
--- scripts/wave_list_level3.gd
+++ scripts/wave_list_level3.gd
@@ -6,25 +6,17 @@ const base_wave_entry = {
 	"timer": null,
 }
 
+# Setting up our enemy waves
 const wave1 = [
 	{
-		"enemyName": "test3",
-		"amount": 5,
+		"enemyName": "bento_box",
+		"amount": 4,
 		"timer": 3
-	}	
+	}
 ]
-
 
 const wave_list = [wave1]
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func get_wave_list():
 	return wave_list
